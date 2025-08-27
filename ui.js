73cd93ec2c04a76d44173b57638f2419f1310677
Christan Fergus <fergusdesignllc@gmail.com>
2025-08-27 12:54:24 -0600

    adding dirs

--- ui.js
+++
@@ -1,8 +0,0 @@
-alert("✅ ui.js loaded!");
-
-window.onmessage = (event) => {
-  alert("📩 UI received a message!");
-
-  const data = event.data.pluginMessage;
-  document.getElementById("output").textContent = JSON.stringify(data, null, 2);
-};
